86604f9239746e2bdc8ae0051b2cd190c8439825
Rodrigo Almeida <rodrigohend@outlook.com>
2025-03-18 22:58:38 +0000

    update

--- src/Routes/PagarmeRoute.ts
+++ src/Routes/PagarmeRoute.ts
@@ -3,12 +3,16 @@ import PagarmeController from '../Controllers/PagarmeController.ts';
 
 const route = express.Router();
 
+//cliente
+route.get('/clients', PagarmeController.ListClients);
+
+//planos
 route.post('/create-plan',PagarmeController.CreatePlan);
 route.get('/plans',PagarmeController.ListPlans);
-route.get('/clients', PagarmeController.ListClients);
 route.put('/edit-item-plan',PagarmeController.EditItemPlan);
 route.delete('/delete/:id',PagarmeController.DeletePlan);
 
+//assinaturas
 route.post('/create-subscription', PagarmeController.CreateSubscription);
 
 export default route;
--- src/services/Pagarme.service.ts
+++ src/services/Pagarme.service.ts
@@ -195,50 +195,7 @@ const ListClients = async () => {
 }
 
 //ASSINATURA
-
  const CreateSubscription = async (data: CustomerData) => {
-
-
-  const tokenizarCartao = async () => {
-    const url = 'https://api.pagar.me/1/transactions';
-  
-    const data = {
-      amount: 1000, // Valor do pagamento em centavos (R$ 10,00)
-      payment_method: 'credit_card',
-      card: {
-        holder_name: 'Tony Stark',
-        number: '4532464862385322',
-        exp_month: 1,
-        exp_year: 30,
-        cvv: '903',
-      },
-    };
-  
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Basic SEU_TOKEN_AQUI', // Substitua com seu token de autenticação
-        },
-        body: JSON.stringify(data),
-      });
-  
-      const result = await response.json();
-  
-      if (response.ok) {
-        console.log('Token do cartão gerado:', result);
-        return result.card.token; // Retorna o token do cartão gerado
-      } else {
-        console.error('Erro ao gerar o token do cartão:', result);
-        return null;
-      }
-    } catch (error) {
-      console.error('Erro na requisição para gerar o token:', error);
-      return null;
-    }
-  };
-
 
   const options = {
     method: 'POST',
