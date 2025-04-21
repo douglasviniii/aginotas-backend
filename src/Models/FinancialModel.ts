002f956c7aaa72cd314e5c67dec767dd68f3cb1c
thersvg <rodrigohend@outlook.com>
2025-04-21 19:51:29 -0300

    up

--- src/Models/FinancialModel.ts
+++ src/Models/FinancialModel.ts
@@ -3,6 +3,11 @@ import mongoose from "mongoose";
 const FinancialSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   customer: {type: Object, require: true},
+  creationDate: {type: String, default: () => {
+    const today = new Date();
+    return today.toISOString().split('T')[0]; // "YYYY-MM-DD"
+  },
+  },
   dueDate: {type: String, require: true},
   status: {type: String, require: true},
   value: {type: Number, require: true},
