1dc4469002238331254161dc211eb320acc1670e
thersvg <rodrigohend@outlook.com>
2025-05-01 15:34:03 -0300

    up

--- src/services/InvoiceService.ts
+++ src/services/InvoiceService.ts
@@ -7,7 +7,9 @@ const UpdateInvoice = (id: string, data: object) => Invoice.findByIdAndUpdate(id
 const FindInvoice = (id: string) => Invoice.findById(id).populate("user").populate("customer");
 const FindInvoiceCustomer = (id: string) => Invoice.find({customer: id}).sort({ _id: -1 }).populate("user").populate("customer");
 const FindInvoiceCustomerAdmin = (id: string) => Invoice.find({user: id}).sort({ _id: -1 }).populate("admin").populate("user");
-const FindInvoices = (id: String) => Invoice.find({user: id}).sort({ _id: -1 }).populate("user").populate("customer");
+
+const FindInvoices = (id: String) => Invoice.find({ user: id, admin: { $exists: false } }).sort({ _id: -1 }).populate("user").populate("customer");
+
 const FindAllInvoices = () => Invoice.find().populate("user").populate("customer");
 const FindLastInvoice = (id: String) => Invoice.findOne({user: id}).sort({ date: -1 }).limit(1);
 
